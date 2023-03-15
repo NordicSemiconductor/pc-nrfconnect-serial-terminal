60dd8a21814ac6cef0bb90871567573a138ecd5f
aadnekar <aadne.karstad@gmail.com>
2023-03-15 12:42:38 +0100

    Remove documentation card
    We do not have any documentation for Serial Terminal at the moment.

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,6 @@ export default () => (
                 SidePanel: TerminalSidePanel,
             },
         ]}
-        documentation={DocumentationSections}
         autoReselectByDefault
     />
 );
