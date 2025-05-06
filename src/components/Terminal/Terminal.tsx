98dcb785ecd880997ea1cc1dca5d63ce0630849f
Svyatoslav <kiev.devel@gmail.com>
2025-05-06 18:21:28 +0200

    update shared 209

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -446,44 +446,44 @@ export default ({
                                     setHistoryLine(undefined);
                                 }
                             }}
-                            // onMouseDown={e => {
-                            //     const isMiddleMouseClick = e.button === 1;
-                            //
-                            //     if (!isMiddleMouseClick) return;
-                            //
-                            //     const hasClipboard =
-                            //         clipboard.readText('clipboard').trim() !==
-                            //         '';
-                            //
-                            //     console.log('onMouseDown e', e);
-                            //     console.log(
-                            //         'lineModeInputRef: selectionStart ',
-                            //         lineModeInputRef.current?.selectionStart
-                            //     );
-                            //
-                            //     if (!hasClipboard) {
-                            //         return;
-                            //     }
-                            //
-                            //     if (historyLine != null) {
-                            //         setCmdLine(
-                            //             `${historyLine}${clipboard.readText(
-                            //                 'clipboard'
-                            //             )}`
-                            //         );
-                            //
-                            //         resetHistoryScroll();
-                            //         setHistoryLine(undefined);
-                            //
-                            //         return;
-                            //     }
-                            //
-                            //     setCmdLine(
-                            //         `${cmdLine}${clipboard.readText(
-                            //             'clipboard'
-                            //         )}`
-                            //     );
-                            // }}
+                            onMouseDown={e => {
+                                const isMiddleMouseClick = e.button === 1;
+
+                                if (!isMiddleMouseClick) return;
+
+                                const hasClipboard =
+                                    clipboard.readText('clipboard').trim() !==
+                                    '';
+
+                                console.log('onMouseDown e', e);
+                                console.log(
+                                    'lineModeInputRef: selectionStart ',
+                                    lineModeInputRef.current?.selectionStart
+                                );
+
+                                if (!hasClipboard) {
+                                    return;
+                                }
+
+                                if (historyLine != null) {
+                                    setCmdLine(
+                                        `${historyLine}${clipboard.readText(
+                                            'clipboard'
+                                        )}`
+                                    );
+
+                                    resetHistoryScroll();
+                                    setHistoryLine(undefined);
+
+                                    return;
+                                }
+
+                                setCmdLine(
+                                    `${cmdLine}${clipboard.readText(
+                                        'clipboard'
+                                    )}`
+                                );
+                            }}
                         />
                     </div>
                     <button
